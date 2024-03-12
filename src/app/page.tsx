1ed6812dd119f5346b3cb628de177bda94029814
yy <yy123@ggg.com>
2024-03-12 12:47:26 +0800

    123

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+"use client";
 import Image from "next/image";
+import {useEffect, useState} from "react";
 
 export default function Home() {
+  const [a] = useState(1)
+  useEffect(()=>{
+    console.log(a)
+  },[])
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
